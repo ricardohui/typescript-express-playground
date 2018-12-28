7e477659362b2692ec27565cb83e62af65947718
Ricardo Hui <rcdhui@gmail.com>
2018-12-28 18:00:42 +0800

    initial commit
    Create an express app that listen on 3002 port

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+app.get("/", (request, response) => {
+  response.send("Hello Worldx1");
+  response.end("Hello World");
+});
+app.listen(3002, () => console.log("Listening on port 3002,.."));
